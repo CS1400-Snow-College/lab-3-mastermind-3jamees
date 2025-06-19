212b9bb4b524930880d8f9e4b2686ba3cc8804fb
3jamees <jamesiiking8@gmail.com>
2025-06-18 20:14:57 -0400

    Instructions for game

--- Program.cs
+++ Program.cs
@@ -3,7 +3,12 @@
 // Date: June 18, 2025
 // Lab: Lab 4 - Mastermind
 
+{
+        // Greeting
+        Console.WriteLine("Welcome to Mastermind!");
+        Console.WriteLine("Guess the 4-letter secret code made of letters a-g (no repeats).");
+        Console.WriteLine("You'll be told how many letters are correct and in the right place,");
+        Console.WriteLine("and how many letters are correct but in the wrong place.");
+        Console.WriteLine();
 
-
-
-    
+        
